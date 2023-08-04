7201b1b3252e050cf8972a7d881e0b16cd535f77
swamiswami03 <swamiswami03@gmail.com>
2023-08-04 15:36:55 -0400

    cleaned up code

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 use sysinfo::{System, SystemExt};
 use colored::*;
+use std::env;
+
 
 fn main() {
-    endeavour();
-    manjaro();
-    arch();
+    newfetch();
 }
 
-fn endeavour() {
+
+fn newfetch() {
+
 
     let sys = System::new_all();
-
-
     // sys name
     let system_name = sys.name();
     // host
@@ -34,32 +34,83 @@ fn endeavour() {
     let minutes_system_uptime = minutes_system_uptime;
     let hours_system_uptime = seconds_system_uptime / 3600.0;
     let hours_system_uptime = hours_system_uptime;
-
+    //packages
+    let installed_packages = std::process::Command::new("pacman")
+        .args(&["-Qq"])
+        .output()
+        .expect("Failed to execute command");
+    let package_output = String::from_utf8_lossy(&installed_packages.stdout);
+    let package_count = package_output.lines().count();
 
 
     // string variables
-    let os = "OS".red();
-    let hostname = "Host Name".red();
-    let kernel = "Kernel".red();
-    let memory = "Memory".red();
-    let cores = "Cores".red();
-    let swap = "Swap".red();
-    let uptime = "Uptime".red();
+    let os = "OS".bright_purple();
+    let hostname = "Host Name".bright_purple();
+    let kernel = "Kernel".bright_purple();
+    let packages = "Packages".bright_purple();
+    let cores = "Cores".bright_purple();
+    let memory = "Memory".bright_purple();
+    let uptime = "Uptime".bright_purple();
 
-    let end1 = "      /\\".blue();
-    let end2 = "    //  \\\\   ".blue();
-    let end3 = "   //    \\ \\".blue();
-    let end4 = " / /     _) )".blue();
-    let end5 = "/_/___-- __- ".blue();
-    let end6 = " /____--     ".blue();
+    // ascii art declaration
 
+    let mut end1: ColoredString = String::new().bright_blue();
+    let mut end2: ColoredString = String::new().bright_blue();
+    let mut end3: ColoredString = String::new().bright_blue();
+    let mut end4: ColoredString = String::new().bright_blue();
+    let mut end5: ColoredString = String::new().bright_blue();
+    let mut end6: ColoredString = String::new().bright_blue();
+    let mut end7: ColoredString = String::new().bright_blue();
 
+    if let Some(name) = system_name {
+        if name == "EndeavourOS" {
+            end1 = "      /\\".bright_blue();
+            end2 = "    //  \\\\   ".bright_blue();
+            end3 = "   //    \\ \\".bright_blue();
+            end4 = " / /     _) )".bright_blue();
+            end5 = "/_/___-- __- ".bright_blue();
+            end6 = " /____--     ".bright_blue();
+            end7 = "             ".bright_blue();
+            println!("{end1}         {}          {}", os, name);
 
-    if let Some(SystemName) = system_name {
-        println!("{end1}         {}          {}",os,SystemName);
-    } else {
-        println!("nothing");
+        } else if name == "Arch" {
+            end1 = "      /\\".cyan();
+            end2 = "     /\\ \\".cyan();
+            end3 = "    /    \\".cyan();
+            end4 = "   /  ,,  \\".cyan();
+            end5 = "  /  |  | -\\".cyan();
+            end6 = " /_''    ''_\\".cyan();
+            end7 = "             ".cyan();
+
+            println!("{end1}         {}          {}", os, name);
+
+        } else if name == "Manjaro"{
+            end1 = "||||||||| ||||".green();
+            end2 = "||||||||| ||||".green();
+            end3 = "||||      ||||".green();
+            end4 = "|||| |||| ||||".green();
+            end5 = "|||| |||| ||||".green();
+            end6 = "|||| |||| ||||".green();
+            end7 = "             ".green();
+
+            println!("{end1}         {}          {}", os, name);
+
+        }  else{
+            end1 = "    .--.".cyan();
+            end2 = "   |o_o |".cyan();
+            end3 = "   |:_/ |".cyan();
+            end4 = "  //    \\".cyan();
+            end5 = " (|     | )".cyan();
+            end6 = "/'/_   _/`\\".cyan();
+            end7 = "\\__)=(___/".cyan();
+
+            println!("{end1}         {}          {}", os, name);
+
+        }
     }
+
+
+
 
     // Host Name
     if let Some(system_host) = system_host {
@@ -74,178 +125,21 @@ fn endeavour() {
     } else {
         println!("nothing");
     }
-
-    // Memory
-    println!("{end4}    {}      {:?}/{:?} GB Used ",memory,system_used_memory,system_total_memory);
+    // Packages
+    println!("{end4}    {}    {}", packages, package_count);
 
     // Cores
     println!("{end5}    {}       {}",cores,sys.cpus().len());
 
-    // Swap
-    //println!("{end6}     {}        {:?}/{:?} Swap Used",swap,sys.used_swap(),sys.total_swap());
-
-    println!("{end6}    {}      {:.1} Hours        ",uptime,hours_system_uptime);
-
-
-
-
-}
-
-fn manjaro() {
-
-    let sys = System::new_all();
-
-
-    // sys name
-    let system_name = sys.name();
-    // host
-    let system_host = sys.host_name();
-    // kernel
-    let system_kernel = sys.kernel_version();
-    // memory
-    let system_total_memory = sys.total_memory();
-    let system_total_memory = system_total_memory/ u64::pow(2,30);
-    let system_used_memory = sys.used_memory();
-    let system_used_memory = system_used_memory/ u64::pow(2,30);
-    let system_total_swap = sys.total_swap();
-    let system_total_swap = system_total_swap/u64::pow(2,30);
-    let system_used_swap = sys.used_swap();
-    let system_used_swap = system_used_swap/u64::pow(2,30);
-    // uptime
-    let seconds_system_uptime = sys.uptime();
-    let seconds_system_uptime = seconds_system_uptime as f32;
-    let minutes_system_uptime = seconds_system_uptime/ 60.0;
-    let minutes_system_uptime = minutes_system_uptime;
-    let hours_system_uptime = seconds_system_uptime / 3600.0;
-    let hours_system_uptime = hours_system_uptime;
-
-
-
-    // string variables
-    let os = "OS".red();
-    let hostname = "Host Name".red();
-    let kernel = "Kernel".red();
-    let memory = "Memory".red();
-    let cores = "Cores".red();
-    let swap = "Swap".red();
-    let uptime = "Uptime".red();
-
-    let end1 = "||||||||| ||||".green();
-    let end2 = "||||||||| ||||".green();
-    let end3 = "||||      ||||".green();
-    let end4 = "|||| |||| ||||".green();
-    let end5 = "|||| |||| ||||".green();
-    let end6 = "|||| |||| ||||".green();
-
-
-
-    if let Some(SystemName) = system_name {
-        println!("{end1}   {}          {}",os,SystemName);
-    } else {
-        println!("nothing");
-    }
-
-    // Host Name
-    if let Some(system_host) = system_host {
-        println!("{end2}   {}   {}",hostname,system_host);
-    } else {
-        println!("nothing");
-    }
-
-    // Kernel
-    if let Some(system_kernel) = system_kernel {
-        println!("{end3}   {}      {}",kernel,system_kernel);
-    } else {
-        println!("nothing");
-    }
-
     // Memory
-    println!("{end4}   {}      {:?}/{:?} GB Used ",memory,system_used_memory,system_total_memory);
+    println!("{end6}    {}      {:?}/{:?} GB Used ",memory,system_used_memory,system_total_memory);
 
-    // Cores
-    println!("{end5}   {}       {}",cores,sys.cpus().len());
 
     // Swap
     //println!("{end6}     {}        {:?}/{:?} Swap Used",swap,sys.used_swap(),sys.total_swap());
 
-    println!("{end6}    {}     {:.1} Hours        ",uptime,hours_system_uptime);
-}
-
-fn arch() {
-    let sys = System::new_all();
+    println!("{end7}    {}      {:.1} Hours        ",uptime,hours_system_uptime);
+    println!("");
 
 
-    // sys name
-    let system_name = sys.name();
-    // host
-    let system_host = sys.host_name();
-    // kernel
-    let system_kernel = sys.kernel_version();
-    // memory
-    let system_total_memory = sys.total_memory();
-    let system_total_memory = system_total_memory/ u64::pow(2,30);
-    let system_used_memory = sys.used_memory();
-    let system_used_memory = system_used_memory/ u64::pow(2,30);
-    let system_total_swap = sys.total_swap();
-    let system_total_swap = system_total_swap/u64::pow(2,30);
-    let system_used_swap = sys.used_swap();
-    let system_used_swap = system_used_swap/u64::pow(2,30);
-    // uptime
-    let seconds_system_uptime = sys.uptime();
-    let seconds_system_uptime = seconds_system_uptime as f32;
-    let minutes_system_uptime = seconds_system_uptime/ 60.0;
-    let minutes_system_uptime = minutes_system_uptime;
-    let hours_system_uptime = seconds_system_uptime / 3600.0;
-    let hours_system_uptime = hours_system_uptime;
-
-
-
-    // string variables
-    let os = "OS".red();
-    let hostname = "Host Name".red();
-    let kernel = "Kernel".red();
-    let memory = "Memory".red();
-    let cores = "Cores".red();
-    let swap = "Swap".red();
-    let uptime = "Uptime".red();
-
-    let end1 = "      /\\".cyan();
-    let end2 = "     /\\ \\".cyan();
-    let end3 = "    /    \\".cyan();
-    let end4 = "   /  ,,  \\".cyan();
-    let end5 = "  /  |  | -\\".cyan();
-    let end6 = " /_''    ''_\\".cyan();
-
-
-
-    if let Some(SystemName) = system_name {
-        println!("{end1}         {}          {}",os,SystemName);
-    } else {
-        println!("nothing");
-    }
-
-    // Host Name
-    if let Some(system_host) = system_host {
-        println!("{end2}        {}   {}",hostname,system_host);
-    } else {
-        println!("nothing");
-    }
-
-    // Kernel
-    if let Some(system_kernel) = system_kernel {
-        println!("{end3}       {}      {}",kernel,system_kernel);
-    } else {
-        println!("nothing");
-    }
-
-    // Memory
-    println!("{end4}      {}      {:?}/{:?} GB Used ",memory,system_used_memory,system_total_memory);
-
-    // Cores
-    println!("{end5}     {}       {}",cores,sys.cpus().len());
-
-    // Swap
-    //println!("{end6}     {}        {:?}/{:?} Swap Used",swap,sys.used_swap(),sys.total_swap());
-
-    println!("{end6}    {}      {:.1} Hours        ",uptime,hours_system_uptime);
 }
